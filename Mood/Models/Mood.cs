62e17d6ce8b46282d0f3d661a5efe5859b13b661
AWA5CG8126DFW\Administrator <isabelle.s.eriksson@gmail.com>
2018-10-12 14:44:19 +0200

    La till skal för Mood

---
+++ Mood/Models/Mood.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mood.Models
+{
+    public class OdeToMood
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Restaurant Name")]
+        [Required, MaxLength(80)]
+        public string Name { get; set; }
+    }
+}
