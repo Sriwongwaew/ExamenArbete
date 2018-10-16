f81ebe778e1f7e02bbf4bab9232a1b16acd553c2
AWA5CG8126DFW\Administrator <isabelle.s.eriksson@gmail.com>
2018-10-16 09:32:37 +0200

    Fixat till HomeControllern

--- Mood/Controllers/HomeController.cs
+++ Mood/Controllers/HomeController.cs
@@ -15,21 +15,6 @@ using System.Web;
 
 namespace Mood.Controllers
 {
-    
-    //public interface IFormFile
-    //{
-    //    string ContentType { get; }
-    //    string ContentDisposition { get; }
-    //    IHeaderDictionary Headers { get; }
-    //    long Length { get; }
-    //    string Name { get; }
-    //    string FileName { get; }
-    //    Stream OpenReadStream();
-    //    void CopyTo(Stream target);
-    //    Task CopyToAsync(Stream target, CancellationToken cancellationToken);
-    //    Task CopyToAsync(FileStream stream);
-    //}
-
     public class HomeController : Controller
     {
         const string subscriptionKey = "0f02fdf50aa34b43a890cc185515e46f";
@@ -41,8 +26,6 @@ namespace Mood.Controllers
         {
             return RedirectToAction("Index", "Mood");
         }
-
-
 
         static async Task<string> MakeAnalysisRequest(string pic)
         {
@@ -94,86 +77,11 @@ namespace Mood.Controllers
             }
         }
 
-        static string JsonPrettyPrint(string json)
-        {
-            if (string.IsNullOrEmpty(json))
-                return string.Empty;
-
-            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
-
-            StringBuilder sb = new StringBuilder();
-            bool quote = false;
-            bool ignore = false;
-            int offset = 0;
-            int indentLength = 3;
-
-            foreach (char ch in json)
-            {
-                switch (ch)
-                {
-                    case '"':
-                        if (!ignore) quote = !quote;
-                        break;
-                    case '\'':
-                        if (quote) ignore = !ignore;
-                        break;
-                }
-
-                if (quote)
-                    sb.Append(ch);
-                else
-                {
-                    switch (ch)
-                    {
-                        case '{':
-                        case '[':
-                            sb.Append(ch);
-                            sb.Append(Environment.NewLine);
-                            sb.Append(new string(' ', ++offset * indentLength));
-                            break;
-                        case '}':
-                        case ']':
-                            sb.Append(Environment.NewLine);
-                            sb.Append(new string(' ', --offset * indentLength));
-                            sb.Append(ch);
-                            break;
-                        case ',':
-                            sb.Append(ch);
-                            sb.Append(Environment.NewLine);
-                            sb.Append(new string(' ', offset * indentLength));
-                            break;
-                        case ':':
-                            sb.Append(ch);
-                            sb.Append(' ');
-                            break;
-                        default:
-                            if (ch != ' ') sb.Append(ch);
-                            break;
-                    }
-                }
-            }
-
-            return sb.ToString().Trim();
-        }
-
         [HttpPost("UploadFiles")]
         public async Task<IActionResult> Post(IFormFile file)
         {
-            //long size = files.Sum(f => f.Length);
 
             var filePath = Path.GetTempFileName();
-
-            //foreach (var formFile in files)
-            //{
-            //    if (formFile.Length > 0)
-            //    {
-            //        using (var stream = new FileStream(filePath, FileMode.Create))
-            //        {
-            //             await formFile.CopyToAsync(stream);
-            //        }
-
-            //    }
-            //}
 
             using (var stream = new FileStream(filePath, FileMode.Create))
             {
