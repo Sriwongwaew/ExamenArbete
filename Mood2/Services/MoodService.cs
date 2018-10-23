aca9f209de5f7575d9962f30cdb426e4c9470e65
ACADEMY-6162WZF\Administrator <Sriwongwaew@gmail.com>
2018-10-23 16:50:32 +0200

    Nyaste versionen.

--- Mood2/Services/MoodService.cs
+++ Mood2/Services/MoodService.cs
@@ -92,37 +92,37 @@ namespace Mood2.Services
              new Playlist //-------
                  {
                  EmotionDataId = angerID.Id,
-                 PlaylistLink = "https://open.spotify.com/user/cosmoph/playlist/1AcFWoqI3Pug3wmy59mCb0?si=1kShR6GjT4uYk6pxpxEcOw"
+                 PlaylistLink = "https://open.spotify.com/embed/user/cosmoph/playlist/1AcFWoqI3Pug3wmy59mCb0"
              },
               new Playlist
               {
                   EmotionDataId = angerID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/jewlgurl/playlist/7L08IETH8EQmm7k4r8rivb?si=RUQvLCwSTqiKw9eaRj4QwA"
+                  PlaylistLink = "https://open.spotify.com/embed/user/jewlgurl/playlist/7L08IETH8EQmm7k4r8rivb"
               },
               new Playlist
               {
                   EmotionDataId = angerID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/5s7Sp5OZsw981I2OkQmyrz?si=oEz6cfkUTIeYAqNUiWMyRg"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/5s7Sp5OZsw981I2OkQmyrz"
               },
               new Playlist
               {
                   EmotionDataId = angerID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/vheist16/playlist/0jFTAWnKeif1jeycmfzntA?si=sMYW68SBRS2NljjAeaMoQg"
+                  PlaylistLink = "https://open.spotify.com/embed/user/vheist16/playlist/0jFTAWnKeif1jeycmfzntA"
               },
               new Playlist
               {
                   EmotionDataId = angerID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/1222144685/playlist/2x1PpZv5jzhUGxzztJwaNi?si=ac5Kjf_6RCKw8Gdj-3tanw"
+                  PlaylistLink = "https://open.spotify.com/embed/user/1222144685/playlist/2x1PpZv5jzhUGxzztJwaNi"
               },
               new Playlist
               {
                   EmotionDataId = angerID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/1222144685/playlist/2x1PpZv5jzhUGxzztJwaNi?si=YrQ6TC6IS3G2qQXVhUDbIA"
+                  PlaylistLink = "https://open.spotify.com/embed/user/119988902/playlist/5qUWjT1fYzj9zwstkkOWjf"
               },
                new Playlist
                {
                    EmotionDataId = angerID.Id,
-                   PlaylistLink = "https://open.spotify.com/user/mdlewis22/playlist/48zs9ygC4jOvfn4sSt9RjB?si=m8Qg2fO9RPGJGAiJTsYD3Q"
+                   PlaylistLink = "https://open.spotify.com/embed/user/mdlewis22/playlist/48zs9ygC4jOvfn4sSt9RjB"
                });
             //-----------------------------------------------------------------------
 
@@ -147,37 +147,37 @@ namespace Mood2.Services
              new Playlist //-------
                  {
                  EmotionDataId = happyID.Id,
-                 PlaylistLink = " "
+                 PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DXaXB8fQg7xif"
              },
               new Playlist
               {
                   EmotionDataId = happyID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX8a1tdzq5tbM"
               },
               new Playlist
               {
                   EmotionDataId = happyID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX0rM1NjYKMJa"
               },
               new Playlist
               {
                   EmotionDataId = happyID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DWSkMjlBZAZ07"
               },
               new Playlist
               {
                   EmotionDataId = happyID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DWYaLsh50zXN6"
               },
               new Playlist
               {
                   EmotionDataId = happyID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DXcQFLaKHy42g"
               },
                new Playlist
                {
                    EmotionDataId = happyID.Id,
-                   PlaylistLink = " "
+                   PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DWTJ7xPn4vNaz"
                });
             //-----------------------------------------------------------------------
 
@@ -201,37 +201,37 @@ namespace Mood2.Services
              new Playlist //-------
                  {
                  EmotionDataId = surpriseID.Id,
-                 PlaylistLink = "https://open.spotify.com/user/126792456/playlist/0feDLyvWBdzOvewFnLM6Px?si=oGdFeumMQKCkgulTpcVsKQ"
+                 PlaylistLink = "https://open.spotify.com/embed/user/126792456/playlist/0feDLyvWBdzOvewFnLM6Px"
              },
               new Playlist
               {
                   EmotionDataId = surpriseID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/officiallifeisstrange/playlist/1f5ZzLDTXHTDR8CYIEddpW?si=rXefv5a9SXq5VBpTxDRArw"
+                  PlaylistLink = "https://open.spotify.com/embed/user/officiallifeisstrange/playlist/1f5ZzLDTXHTDR8CYIEddpW"
               },
               new Playlist
               {
                   EmotionDataId = surpriseID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/rocky-m/playlist/54pucq433rZsjpuK9GdbaD?si=w5VQfeqUSziPdJ8jUgIy5w"
+                  PlaylistLink = "https://open.spotify.com/embed/user/rocky-m/playlist/54pucq433rZsjpuK9GdbaD"
               },
               new Playlist
               {
                   EmotionDataId = surpriseID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DWUraJYejk11q?si=FAIIuLeBSpKuc2sEA1tVyw"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DWUraJYejk11q"
               },
               new Playlist
               {
                   EmotionDataId = surpriseID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX3ZeFHRhhi7Y?si=_p25S9lQSIK3YnI9zcBJ2Q"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX3ZeFHRhhi7Y"
               },
               new Playlist
               {
                   EmotionDataId = surpriseID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/ofinns/playlist/1fNw33uLdD1tSc0MYk46bb?si=m5MNmCYmRRCDudpBJhrtBw"
+                  PlaylistLink = "https://open.spotify.com/embed/user/ofinns/playlist/1fNw33uLdD1tSc0MYk46bb"
               },
                new Playlist
                {
                    EmotionDataId = surpriseID.Id,
-                   PlaylistLink = "https://open.spotify.com/user/128899670/playlist/5NtjgKz4doejP5HJtKXFcS?si=EDbQVHBmTcKDekm1BPzxrA"
+                   PlaylistLink = "https://open.spotify.com/embed/user/128899670/playlist/5NtjgKz4doejP5HJtKXFcS"
                });
 
 
@@ -257,37 +257,37 @@ namespace Mood2.Services
              new Playlist //-------
                  {
                  EmotionDataId = sadnessID.Id,
-                 PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX2ViysbCzMHO?si=DaBuAgXlRhqjtxz3aHA4Lw"
+                 PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX2ViysbCzMHO"
              },
               new Playlist
               {
                   EmotionDataId = sadnessID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX4sWSpwq3LiO?si=AniopkLURw2Fk5kiU7C59Q"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX4sWSpwq3LiO"
               },
               new Playlist
               {
                   EmotionDataId = sadnessID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX7qK8ma5wgG1?si=JT1WvxumS9KxIEp_3vm6Cg"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX7qK8ma5wgG1"
               },
               new Playlist
               {
                   EmotionDataId = sadnessID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX4Umko6nOmN7?si=ocrJ-61fSi-ZEopGeh6Djw"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX4Umko6nOmN7"
               },
               new Playlist
               {
                   EmotionDataId = sadnessID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DWVHi578arQLF?si=FLFh3GpUSMWttch7Vdcfpw"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DWVHi578arQLF"
               },
               new Playlist
               {
                   EmotionDataId = sadnessID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX0jgyAiPl8Af?si=nZJ1ClGSQPqp__TUwiPN_A"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX0jgyAiPl8Af"
               },
                new Playlist
                {
                    EmotionDataId = sadnessID.Id,
-                   PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXcTFqUWLeE6m?si=-vPuLhwqS3uUNKPEX2-8ag"
+                   PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DXcTFqUWLeE6m"
                });
 
             //--------------------------------------------------
@@ -311,37 +311,37 @@ namespace Mood2.Services
              new Playlist //-------
                  {
                  EmotionDataId = fearID.Id,
-                 PlaylistLink = " "
+                 PlaylistLink = "https://open.spotify.com/embed/user/1222144685/playlist/2x1PpZv5jzhUGxzztJwaNi"
              },
               new Playlist
               {
                   EmotionDataId = fearID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX82SvZY6fOWx"
               },
               new Playlist
               {
                   EmotionDataId = fearID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/user/1284086214/playlist/2cfr9ecGedQM3WhxRWZtLE"
               },
               new Playlist
               {
                   EmotionDataId = fearID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/user/hauntedwisconsin.com/playlist/23NaNmH6LuCctSM6lplaZb"
               },
               new Playlist
               {
                   EmotionDataId = fearID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/user/128899670/playlist/2tvNusayRwIpqd55BA9y1T"
               },
               new Playlist
               {
                   EmotionDataId = fearID.Id,
-                  PlaylistLink = " "
+                  PlaylistLink = "https://open.spotify.com/embed/album/2kUJgoPA2iz9c2jzUr0y1x"
               },
                new Playlist
                {
                    EmotionDataId = fearID.Id,
-                   PlaylistLink = " "
+                   PlaylistLink = "https://open.spotify.com/embed/show/4CSrmtPagkWuNGy4JocfNB"
                });
 
             //--------------------------------------------------------------
@@ -365,37 +365,37 @@ namespace Mood2.Services
              new Playlist //-------
                  {
                  EmotionDataId = neutralID.Id,
-                 PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX4WYpdgoIcn6?si=Dvsd5HsDSwGay9Cg8Lb9Gw"
+                 PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX4WYpdgoIcn6"
              },
               new Playlist
               {
                   EmotionDataId = neutralID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DWVYZVgDjIR7c?si=bsa4-gDPTjmaJAOcMz9_Tg"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DWVYZVgDjIR7c"
               },
               new Playlist
               {
                   EmotionDataId = neutralID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXcBWIGoYBM5M?si=ZPbjmypOSg-IusRuJ_9KAg"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DXcBWIGoYBM5M"
               },
               new Playlist
               {
                   EmotionDataId = neutralID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXc4CrXgl8cum?si=uXpKr0wvS2-iYUHpyiJO9g"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DXc4CrXgl8cum"
               },
               new Playlist
               {
                   EmotionDataId = neutralID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXcF6B6QPhFDv?si=VCFc1vVLRGWY7b9VU6-n4Q"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DXcF6B6QPhFDv"
               },
               new Playlist
               {
                   EmotionDataId = neutralID.Id,
-                  PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXcF6B6QPhFDv?si=d5B4575kRdS6UOLi0UJ0Lg"
+                  PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX1a3nBJMXoti"
               },
                new Playlist
                {
                    EmotionDataId = neutralID.Id,
-                   PlaylistLink = "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXaXB8fQg7xif?si=pc6zz6j4QhWyTAFPkkam4A"
+                   PlaylistLink = "https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DXaXB8fQg7xif"
                });
             _context.SaveChanges();
         }
